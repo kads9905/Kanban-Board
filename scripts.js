55b52f34fda667ba28efa8a42969eb0fe1586d39
kads9905 <kadambari9905@gmail.com>
2026-06-23 13:34:11 +0530

    Persist tasks using local storage

--- scripts.js
+++ scripts.js
@@ -35,8 +35,35 @@ function updateTaskCount() {
         const tasks = col.querySelectorAll(".task");
         const count = col.querySelector(".right");
 
+        tasksData[col.id] = Array.from(tasks).map(t => {
+            return {
+                title: t.querySelector("h2").innerText,
+                desc: t.querySelector("p").innerText
+            };
+        });
+
+        localStorage.setItem("tasks", JSON.stringify(tasksData));
+
         count.innerText = tasks.length;
     });
+}
+
+
+if (localStorage.getItem("tasks")) {
+
+    const data = JSON.parse(localStorage.getItem("tasks"));
+
+    for (const col in data) {
+
+        const column = document.querySelector(`#${col}`);
+
+        data[col].forEach(task => {
+            addTask(task.title, task.desc, column);
+        });
+
+    }
+
+    updateTaskCount();
 }
 
 function addDragEventsOnColumn(column) {
